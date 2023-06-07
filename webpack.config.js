92e9d63e271d6a7a8b1c416167f824b8b27be56a
pck <3500146682@qq.com>
2023-06-07 23:22:20 +0800

    css

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,22 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 var path = require('path');
 
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
     filename: '[name].[contenthash].js'
+  },
+  plugins: [new HtmlWebpackPlugin({
+    title: 'PCK',
+    template: 'src/assets/index.html'
+  })],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
   }
 };
